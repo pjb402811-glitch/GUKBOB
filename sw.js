26a3c12e045ec6a9a7f1c6f83c9178389973dbad
pjb402811-glitch <pjb402811@gmail.com>
2025-11-06 21:38:43 +0900

    ㅇ아이콘

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gukbap-recipe-cache-v11'; // Cache version updated
+const CACHE_NAME = 'gukbap-recipe-cache-v12'; // Cache version updated
 const urlsToCache = [
   '/',
   '/index.html',
